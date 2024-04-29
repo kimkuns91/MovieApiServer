7307a4a9001481aa8ab61213588ba70a303b8274
yonding <uwrgoy7584@khu.ac.kr>
2024-04-29 17:36:27 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+import { config } from "dotenv";
+import cors from "cors";
+import express from "express";
+
+config();
+
+const port = process.env.PORT || 9000;
+const app = express();
+
+app.use(express.json());
+app.use(
+    cors({
+        origin: "*",
+    })
+);
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        status: "success",
+        message: "Hello World",
+    });
+});
+
+app.all("*", (req, res) => {
+    res.status(404).send("Sorry, the route you are going to does not exist");
+});
+
+app.listen(port, () => {
+    console.info(`Server running on port ${port}`);
+});
+
+export default app;
